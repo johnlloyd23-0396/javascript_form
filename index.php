8d5591b2be971f92e034fde796efe8ec0b4d9b71
amoyot <jlamoyot23-0396@cca.edu.ph>
2025-02-17 18:34:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,42 @@
 	</head>
 	<body style>
 		<h1> Student Record </h1>
-		<table id="student_table">
+		<table id="studentTable" border = 1>
 		</table>
 		
 		<script>
 			const studentTable = document
-				.getElementById("student_table");
+				.getElementById("studentTable");
 				
 			console.log(studentTable);
+			
 			const row1 = studentTable.insertRow();
 			const row1col1= row1.insertCell();
 			const row1col2= row1.insertCell();
 			
 			row1col1.innerHTML = "LAST NAME";
 			row1col2.innerHTML = "FIRST NAME";
+			
+			const row2 = studentTable.insertRow();
+			const row2col1= row2.insertCell();
+			const row2col2= row2.insertCell();
+			
+			const inputLastName = document.createElement("input");
+			inputLastName.type ="text";
+			inputLastName.placeholder="Insert Last Name";
+			
+			row2col1.append(inputLastName);
+			
+			const inputFirstName = document.createElement("input");
+			inputFirstName.type ="text";
+			inputFirstName.placeholder="Insert First Name";
+			row2col2.append(inputFirstName);
+			
+			const row3 = studentTable.insertRow();
+			const row3col1 = row3.insertCell();
+			row3col1.colSpan="2";
+			row3col1.innerHTML="TEST";
+			
 		</script>
 	</body>
 </html>
