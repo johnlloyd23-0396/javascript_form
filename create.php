a8ed4da220605ffc916b630a3229eb5c49be9a00
amoyot <jlamoyot23-0396@cca.edu.ph>
2025-02-17 19:37:52 +0800

    createlast

--- create.php
+++ create.php
@@ -1,6 +1,9 @@
 <?php
 	echo "Hello World";
 	var_dump($_POST);
+	$lastname = $_POST['last_name'];
+	$firstname = $_POST['first_name'];
+?>
 ?>
 <br>
 <br>
@@ -18,5 +21,17 @@
 	  die("Connection failed: " . $conn->connect_error);
 	}
 	echo "Connected successfully";
+	
+	$sql = "INSERT INTO students (id,last_name,first_name)
+			VALUES ('', '$lastname', '$firstname')";
+
+			if ($conn->query($sql) === TRUE) {
+			  echo "<br>";
+			  echo "<br>";
+			  echo "<br>";
+			  echo "New record created successfully";
+			} else {
+			  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 ?>
