6cc8c15c94a20ee9aab889b32b9e801be129493a
amoyot <jlamoyot23-0396@cca.edu.ph>
2025-02-17 19:25:15 +0800

    create2

--- create.php
+++ create.php
@@ -2,3 +2,21 @@
 	echo "Hello World";
 	var_dump($_POST);
 ?>
+<br>
+<br>
+<br>
+<?php	
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "mari";
+	// Create connection
+	$conn = new mysqli($servername, $username, $password,$database);
+
+	// Check connection
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+	echo "Connected successfully";
+
+?>
