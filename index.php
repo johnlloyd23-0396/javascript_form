2bef0abad9578250fc356c92963f749f2cf275b6
amoyot <jlamoyot23-0396@cca.edu.ph>
2025-02-17 17:51:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title> Javascript Form </title>
+	</head>
+	<body>
+		<h1> Student Record </h1>
+		<table id="student_table">
+		</table>
+		
+		<script>
+			const studentTable = document
+				.getElementById("student_table");
+				
+			console.log(studentTable);
+		</script>
+	</body>
+</html>
